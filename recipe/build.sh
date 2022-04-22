b28820ddd3d1ab90b94a130d8c1db8b5a8763cc2
Johan Hattne <johan@hattne.se>
2022-04-22 10:04:09 -0700

    Add TVIPS tools 0.1.0-dev.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
-echo "v0.1.0-dev.0-0-g20b9ca1-experimental/jiffies" \
+echo "v0.1.0-dev.1-0-g00fc275-experimental/jiffies" \
     > "${SRC_DIR}/TVIPS-TOOLS-VERSION-FILE"
 rm "${SRC_DIR}/VERSION"
 
+test -n "${OSX_ARCH}" && \
+    iconv_defines="-DIconv_INCLUDE_DIR:PATH=/usr/include -DIconv_LIBRARY:PATH=/usr/lib/libiconv.dylib"
 cmake ${CMAKE_ARGS}                              \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
+    ${iconv_defines}                             \
     "${SRC_DIR}"
 
 cmake --build . --parallel "${CPU_COUNT}"
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 "${PREFIX}/bin/tiff2smv"              \
-    -a -o "t_###.img" -z EST5EDT      \
+    -a -g 5 -o "t_###.img" -z EST5EDT \
     "${SRC_DIR}/test/movie23_000.tif"
 cat << EOF | md5sum -c -
 29a3d5c42f0a2a05312a587934f1ab76  t_000.img
 EOF
 
 "${PREFIX}/bin/tvips2smv"               \
-    -a -o "t_###.img" -z EST5EDT        \
+    -a -g 5 -o "t_###.img" -z EST5EDT   \
     "${SRC_DIR}/test/movie23_000.tvips"
 cat << EOF | md5sum -c -
 29a3d5c42f0a2a05312a587934f1ab76  t_000.img
-5b8e05d443eea3ebc0a6c62965137d30  t_001.img
+6e3d98bf09a8796f2c47609af97d160a  t_001.img
 EOF
