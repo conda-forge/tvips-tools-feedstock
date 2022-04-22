3fac5c0893334d552e59491f454957b08c53d5b6
Johan Hattne <johan@hattne.se>
2022-04-22 10:59:16 -0700

    Add TVIPS tools 0.0.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
-echo "v0.0.2-0-g5579813-master" \
+echo "v0.0.3-0-g6375bc3-master"             \
     > "${SRC_DIR}/TVIPS-TOOLS-VERSION-FILE"
 rm "${SRC_DIR}/VERSION"
 
+test -n "${OSX_ARCH}" && \
+    iconv_defines="-DIconv_INCLUDE_DIR:PATH=/usr/include -DIconv_LIBRARY:PATH=/usr/lib/libiconv.dylib"
 cmake ${CMAKE_ARGS}                              \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
+    ${iconv_defines}                             \
     "${SRC_DIR}"
 
 cmake --build . --parallel "${CPU_COUNT}"
