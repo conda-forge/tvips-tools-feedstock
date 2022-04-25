3ff2e57a31cb984ac32a62de0c0d0919f18288a5
Johan Hattne <johan@hattne.se>
2022-04-25 09:07:41 -0700

    Clean up iconv linking debugging output For testing conda-forge build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,6 @@ echo "v0.1.0-dev.1-0-g00fc275-experimental/jiffies" \
     > "${SRC_DIR}/TVIPS-TOOLS-VERSION-FILE"
 rm "${SRC_DIR}/VERSION"
 
-#test -n "${OSX_ARCH}" && \
-#    iconv_defines="-DIconv_INCLUDE_DIR:PATH=/usr/include -DIconv_LIBRARY:PATH=/usr/lib/libiconv.dylib"
 cmake ${CMAKE_ARGS}                              \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
@@ -15,22 +13,6 @@ cmake ${CMAKE_ARGS}                              \
 cmake --build . --parallel "${CPU_COUNT}"
 cmake --build . --parallel "${CPU_COUNT}" --target man
 cmake --install . --prefix "${PREFIX}"
-
-echo "dumpframe"
-test -n "${OSX_ARCH}" && otool -L dumpframe
-test -z "${OSX_ARCH}" && ldd dumpframe
-
-echo "tiff2smv"
-test -n "${OSX_ARCH}" && otool -L tiff2smv
-test -z "${OSX_ARCH}" && ldd tiff2smv
-
-echo "tvips2smv"
-test -n "${OSX_ARCH}" && otool -L tvips2smv
-test -z "${OSX_ARCH}" && ldd tvips2smv
-
-echo "idoc2smv"
-test -n "${OSX_ARCH}" && otool -L idoc2smv
-test -z "${OSX_ARCH}" && ldd idoc2smv
 
 install -D                               \
     "${SRC_DIR}/README"                  \
