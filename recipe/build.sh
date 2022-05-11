88b9d57124c231566fe0f9861b87e8a1a46e0a35
Johan Hattne <hattne@users.noreply.github.com>
2022-05-11 14:30:50 -0700

    TVIPS tools 0.0.3+1 (#12)
    * Correct recipe's CMake dependency
    The --install option was introduced in CMake 3.15; older versions should
    use "make install".
    
    * Fix pthreads underlinking on Windows
    `tiff2smv -V` failed with no output, `tvips2smv -V` was fine.
    
    * Bump build number
    
    * From microed-tools review: do not use absolute and incorrect path to binaries while testing
    
    * From microed-tools review: more sensible permissions on installed README
    
    * From microed-tools review: use separate microed-data package for test data
    
    * From microed-tools: always use full path to iconv.h
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.09.07.08.30

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,17 @@
 #! /bin/sh
 
+iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
+if test -n "${LD_RUN_PATH}"; then
+    iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
+elif test -n "${OSX_ARCH}"; then
+    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
+fi
+
 echo "v0.0.3-0-g6375bc3-master"             \
     > "${SRC_DIR}/TVIPS-TOOLS-VERSION-FILE"
 rm "${SRC_DIR}/VERSION"
 
-cmake ${CMAKE_ARGS}                              \
+cmake ${CMAKE_ARGS} ${iconv_args}                \
     -DCMAKE_C_FLAGS:STRING="${CFLAGS} -Wall"     \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
     "${SRC_DIR}"
@@ -13,6 +20,6 @@ cmake --build . --parallel "${CPU_COUNT}"
 cmake --build . --parallel "${CPU_COUNT}" --target man
 cmake --install . --prefix "${PREFIX}"
 
-install -D                               \
+install -D -m 644                        \
     "${SRC_DIR}/README"                  \
     "${PREFIX}/share/${PKG_NAME}/README"
