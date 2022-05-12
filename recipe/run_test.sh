978c671c7c09164c2a740e1ffb0f37698678548a
Johan Hattne <hattne@users.noreply.github.com>
2022-05-11 20:21:08 -0700

    TVIPS tools 0.1.0 dev.3+1 (#13)
    * Add TVIPS tools 0.0.1 (#4)
    
    * Dev (#1)
    
    * Add TVIPS tools 0.0.1-dev.0 in dev branch
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.13.14.53.36
    
    * CMake needs a build program
    
    * Remove VERSION before compiling to avoid clash with clang on case-insensitive macOS
    
    * Add TVIPS tools 0.0.1-dev.1 in dev branch (#2)
    
    * Add TVIPS tools 0.0.1-dev.2 (#3)
    
    * Add TVIPS tools 0.0.1-dev.2 in dev branch
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.16.05.14.45
    
    * Fix quoting on Windows
    
    * Add TVIPS tools 0.0.1
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.18.11.42.44
    
    * Add TVIPS tools 0.0.2 (#7)
    
    * Dev (#1)
    
    * Add TVIPS tools 0.0.1-dev.0 in dev branch
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.13.14.53.36
    
    * CMake needs a build program
    
    * Remove VERSION before compiling to avoid clash with clang on case-insensitive macOS
    
    * Add TVIPS tools 0.0.1-dev.1 in dev branch (#2)
    
    * Add TVIPS tools 0.0.1-dev.2 (#3)
    
    * Add TVIPS tools 0.0.1-dev.2 in dev branch
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.16.05.14.45
    
    * Fix quoting on Windows
    
    * Add TVIPS tools 0.0.2
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.20.14.53.59
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.21.23.08.27
    
    * Nitpicks from main branch
    
    * Add TVIPS tools 0.0.3 (#11)
    
    * Add TVIPS tools 0.0.3
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.21.23.08.27
    
    * Nitpicks from dev branch
    
    * Unbreak iconv linking at conda-forge
    
    * TVIPS tools 0.0.3+1 (#12)
    
    * Correct recipe's CMake dependency
    The --install option was introduced in CMake 3.15; older versions should
    use "make install".
    
    * Fix pthreads underlinking on Windows
    `tiff2smv -V` failed with no output, `tvips2smv -V` was fine.
    
    * Bump build number
    
    * From microed-tools review: do not use absolute and incorrect path to binaries while testing
    
    * From microed-tools review: more sensible permissions on installed README
    
    * From microed-tools review: use separate microed-data package for test data
    
    * From microed-tools: always use full path to iconv.h
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.09.07.08.30
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.12.00.18.15
    
    * Spacing, nitpicks

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,22 +1,22 @@
 #! /bin/sh
 
-"${PREFIX}/bin/idoc2smv"                 \
-    -a -f -g 5 -o "t_###.img" -z EST5EDT \
-    "${SRC_DIR}/test/movie23.idoc"
+idoc2smv                                        \
+    -a -f -g 5 -o "t_###.img" -z EST5EDT        \
+    "${PREFIX}/share/microed-data/movie23.idoc"
 cat << EOF | md5sum -c -
 9f01e41a1e01ad59668450160c873214  t_000.img
 EOF
 
-"${PREFIX}/bin/tiff2smv"                 \
-    -a -f -g 5 -o "t_###.img" -z EST5EDT \
-    "${SRC_DIR}/test/movie23_000.tif"
+tiff2smv                                           \
+    -a -f -g 5 -o "t_###.img" -z EST5EDT           \
+    "${PREFIX}/share/microed-data/movie23_000.tif"
 cat << EOF | md5sum -c -
 9f01e41a1e01ad59668450160c873214  t_000.img
 EOF
 
-"${PREFIX}/bin/tvips2smv"                \
-    -a -f -g 5 -o "t_###.img" -z EST5EDT \
-    "${SRC_DIR}/test/movie23_000.tvips"
+tvips2smv                                            \
+    -a -f -g 5 -o "t_###.img" -z EST5EDT             \
+    "${PREFIX}/share/microed-data/movie23_000.tvips"
 cat << EOF | md5sum -c -
 9f01e41a1e01ad59668450160c873214  t_000.img
 18e37588ab86e294124183998f3bf73c  t_001.img
