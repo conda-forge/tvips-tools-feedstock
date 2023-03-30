5ae4dc4b4c3f569e89c710e2ad78cd1e11f284d8
Johan Hattne <johan@hattne.se>
2023-03-30 00:34:20 -0700

    Explicitly call libxml2's activate.bat on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,10 @@ echo v0.1.0-dev.3-0-g984df1b-experimental/jiffies ^
     > "%SRC_DIR%\TVIPS-TOOLS-VERSION-FILE"
 del "%SRC_DIR%\VERSION"
 
+
+:: This should not be necessary.
+call "%CONDA_PREFIX%\etc\conda\activate.d\libxml2_activate.bat"
+
 cmake -G "MinGW Makefiles" %CMAKE_ARGS%                               ^
     -DCMAKE_C_FLAGS:STRING="%CFLAGS% -D_POSIX_C_SOURCE=200809L -Wall" ^
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF                                   ^
