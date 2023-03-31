6162bf43b39d6cb49d2b7dd3627ecf0c42cce0ff
Johan Hattne <johan@hattne.se>
2023-03-30 20:00:35 -0700

    Revert 4e79b812452736cc380b5f6698edf93e83ceda47

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,7 @@ del "%SRC_DIR%\VERSION"
 
 
 :: This should not be necessary.
-:: set XML_CATALOG_FILES=%CONDA_PREFIX%\share\docbook-xsl\catalog.xml
-set XML_CATALOG_FILES=%CONDA_PREFIX%\etc\xml\catalog
+set XML_CATALOG_FILES=%CONDA_PREFIX%\share\docbook-xsl\catalog.xml
 
 cmake -G "MinGW Makefiles" %CMAKE_ARGS%                               ^
     -DCMAKE_C_FLAGS:STRING="%CFLAGS% -D_POSIX_C_SOURCE=200809L -Wall" ^
