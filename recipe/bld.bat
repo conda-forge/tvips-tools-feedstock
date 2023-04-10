19f44374e3e5d9789ba9b124778f14cba4ab0acc
Johan Hattne <hattne@users.noreply.github.com>
2023-04-10 15:51:29 -0700

    Disable noarch in docbook-xsl: no need to set XML_CATALOG_FILES on win-64 (#20)
    * Attempt to further debug XML_CATALOG_FILES issue
    
    * Bump build number
    
    * Clean up

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,17 +4,6 @@ echo v0.1.0-dev.3-0-g984df1b-experimental/jiffies ^
     > "%SRC_DIR%\TVIPS-TOOLS-VERSION-FILE"
 del "%SRC_DIR%\VERSION"
 
-
-:: This should not be necessary.
-echo M1
-dir %CONDA_PREFIX%
-echo M2
-dir %CONDA_PREFIX%\etc
-echo M3
-dir %CONDA_PREFIX%\etc\xml
-echo M4
-set XML_CATALOG_FILES=%CONDA_PREFIX%\share\docbook-xsl\catalog.xml
-
 cmake -G "MinGW Makefiles" %CMAKE_ARGS%                               ^
     -DCMAKE_C_FLAGS:STRING="%CFLAGS% -D_POSIX_C_SOURCE=200809L -Wall" ^
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF                                   ^
