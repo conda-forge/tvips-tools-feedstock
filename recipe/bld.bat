3dda1d51122ce34e81f153c29a5f6e8b3590c5ac
Johan Hattne <johan@hattne.se>
2023-04-10 15:31:35 -0700

    Clean up

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,20 +4,6 @@ echo v0.1.0-dev.3-0-g984df1b-experimental/jiffies ^
     > "%SRC_DIR%\TVIPS-TOOLS-VERSION-FILE"
 del "%SRC_DIR%\VERSION"
 
-
-:: This should not be necessary.
-echo M1
-dir %CONDA_PREFIX%
-echo M2
-dir %CONDA_PREFIX%\etc
-echo M3
-dir %CONDA_PREFIX%\etc\xml
-echo M4
-set | findstr catalog
-echo M5
-:: set XML_CATALOG_FILES=%CONDA_PREFIX%\share\docbook-xsl\catalog.xml
-echo M6
-
 cmake -G "MinGW Makefiles" %CMAKE_ARGS%                               ^
     -DCMAKE_C_FLAGS:STRING="%CFLAGS% -D_POSIX_C_SOURCE=200809L -Wall" ^
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF                                   ^
