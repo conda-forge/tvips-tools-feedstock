a21bbc9c6cd129e6ba508e0e9ccd3bd7d01b14f3
Johan Hattne <johan@hattne.se>
2023-10-18 12:46:14 -0700

    Use text-based stubs with 11.0 SDK

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,13 @@ iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
 if test -n "${LD_RUN_PATH}"; then
     iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
 elif test -n "${OSX_ARCH}"; then
-    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.tbd"
+    # Below for MACOSX_DEPLOYMENT_TARGET=11.0 -- for 10.9 use .dylib
+    # instead of .tbd
+    if test "${MACOSX_DEPLOYMENT_TARGET}" = "11.0"; then
+        iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.tbd"
+    else
+        iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
+    fi
 fi
 
 echo "v0.0.3-0-g6375bc3-master"             \
