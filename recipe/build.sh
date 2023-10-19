2dcb39975d749f5cb9c3de56ba359f94057a1d7c
Johan Hattne <johan@hattne.se>
2023-10-19 08:25:41 -0700

    It appears text-based stub libraries were introduced in SDK 10.11

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,14 @@ iconv_args="-DIconv_INCLUDE_DIR:PATH=${CONDA_BUILD_SYSROOT}/usr/include"
 if test -n "${LD_RUN_PATH}"; then
     iconv_args="${iconv_args} -DIconv_IS_BUILT_IN:BOOL=TRUE"
 elif test -n "${OSX_ARCH}"; then
-    # Below for MACOSX_DEPLOYMENT_TARGET=11.0 -- for 10.9 use .dylib
-    # instead of .tbd
-    if test "${MACOSX_DEPLOYMENT_TARGET}" = "11.0"; then
-        iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.tbd"
+    major=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^\([0-9]\+\).*/\1/"`
+    minor=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^[0-9]\+\.\([0-9]\+\).*/\1/"`
+    if test "${major}" -gt "10" -o ("${major}" -eq "10" -a "${minor}" -ge "11"); then
+        libiconv="libiconv.tbd"
     else
-        iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/libiconv.dylib"
+        libiconv="libiconv.dylib"
     fi
+    iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/${libiconv}"
 fi
 
 echo "v0.0.3-0-g6375bc3-master"             \
