e76546870614d2807c37f7f4e5fca3dacc09608c
Johan Hattne <johan@hattne.se>
2023-11-08 21:31:12 -0800

    Synchronize with microed-tools-feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,11 @@ if test -n "${LD_RUN_PATH}"; then
 elif test -n "${OSX_ARCH}"; then
     major=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^\([0-9]*\).*/\1/"`
     minor=`echo "${MACOSX_DEPLOYMENT_TARGET}" | sed -e "s/^[0-9]*\.\([0-9]*\).*/\1/"`
-    if test "${major}" -gt "10" -o \( "${major}" -eq "10" -a "${minor}" -ge "11" \); then
-        libiconv="libiconv.tbd"
-    else
+    if test "${major}" -lt "10" -o \(                           \
+            "${major}" -eq "10" -a "${minor}" -lt "11" \); then
         libiconv="libiconv.dylib"
+    else
+        libiconv="libiconv.tbd"
     fi
     iconv_args="${iconv_args} -DIconv_LIBRARY:PATH=${CONDA_BUILD_SYSROOT}/usr/lib/${libiconv}"
 fi
@@ -23,8 +24,8 @@ cmake ${CMAKE_ARGS} ${iconv_args}                \
     -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS} -Wall" \
     "${SRC_DIR}"
 
-cmake --build . --parallel "${CPU_COUNT}"
-cmake --build . --parallel "${CPU_COUNT}" --target man
+cmake --build .
+cmake --build . --target man
 cmake --install . --prefix "${PREFIX}"
 
 install -D -m 644                        \
